64c12d5d5d888d6f5c03082f871ad086eaa1e271
NoahGav <noahgavdev@gmail.com>
2023-10-24 16:06:47 -0500

    Added a comprehensive doc comment.

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,103 @@ struct Node<Q, R> {
 
 type QueryNodeMap<Q, R> = Arc<ConcurrentMap<Q, Arc<OnceLock<Node<Q, R>>>>>;
 
+/// The `Graph` struct represents a concurrent query dependency graph. It provides
+/// the infrastructure for managing, resolving, and optimizing a wide range of
+/// queries across a variety of applications, including but not limited to
+/// concurrent incremental compilers.
+///
+/// # Overview
+///
+/// A `Graph` instance serves as the central data structure for tracking and managing
+/// dependencies between queries (`Q`) and their respective results (`R`). This data
+/// structure is completely agnostic to the specific use case, and its generic nature
+/// makes it adaptable to a multitude of scenarios beyond compiler optimization.
+///
+/// # Query Resolution
+///
+/// The `Graph` type allows for concurrent query resolution, where queries can be
+/// resolved in parallel, enhancing performance significantly. The `increment` method
+/// does not block and returns a new iteration of the graph immediately. In fact,
+/// the `Graph.query` method is designed to block as little as possible. In the
+/// general case it will block extremely infrequently. This means that queries
+/// will be able to continue chugging away as fast as possible with little to no
+/// interruptions.
+///
+/// # Structure
+///
+/// - `new`: A `QueryNodeMap` representing the current state of queries for the
+///   current iteration. All new queries and their results are stored in this map.
+///   It begins each iteration as an empty map.
+///
+/// - `old`: A `QueryNodeMap` serving as a reference to the map from the previous
+///   iteration. It is used for validating queries and their results from the
+///   current iteration. This reference mechanism provides an efficient way to
+///   track and compare query changes across iterations.
+///
+/// - `resolver`: An associated type (`ResolveQuery`) used to resolve queries
+///   and obtain their results. This type may carry its own state as long as it
+///   implements the `Sync` and `Send` traits, enabling it to work seamlessly in a
+///   multithreaded environment.
+///
+/// # Incremental Compilation
+///
+/// In the context of a concurrent incremental compiler, only queries that are
+/// determined to be outdated are resolved again. This approach greatly reduces the
+/// time required to resolve queries, making the example compiler incremental.
+/// The semantic model is still built every time you mutate the compiler's state,
+/// but most of the work is already done and can be reused, resulting in extremely
+/// fast model building.
+///
+/// # Usage
+///
+/// The `Graph` type can be employed in a wide range of applications, from compilers
+/// to data processing pipelines and more. Its flexibility allows developers to adapt
+/// it to their specific use cases.
+///
+/// # Examples
+///
+/// The `Graph` type can be used in various applications. Below is an example of how
+/// it can be utilized in a concurrent incremental compiler, although its potential
+/// extends far beyond this use case:
+///
+/// ```rust
+/// use query_graph::Graph;
+///
+/// // Create a new Graph instance with a specific resolver.
+/// let graph = Graph::new(compiler_state);
+///
+/// // Query the graph to obtain the result for a specific query.
+/// let result = graph.query(MyQuery);
+///
+/// // Use the result for further processing.
+/// println!("{:?}", result);
+///
+/// // Create a new iteration of the graph by calling increment
+/// // with the new state.
+/// let new_graph = graph.increment(new_state);
+///
+/// // Query the new graph to obtain the result. Because the graph
+/// // tracks query dependencies this query will be very fast to
+/// // resolve. This is what makes the compiler incremental.
+/// let result = new_graph.query(MyQuery);
+///
+/// // Use the result for further processing.
+/// println!("{:?}", result);
+///
+/// ```
+///
+/// In this example, the `Graph` is instantiated with a custom compiler resolver and
+/// is used to resolve queries efficiently. The `ResolveQuery` trait is implemented
+/// for the compiler domain, ensuring that queries are handled appropriately.
+///
+/// # Considerations
+///
+/// - The `Graph` is a versatile data structure that can be applied in various
+///   scenarios beyond compilers. Its flexibility allows developers to adapt it to
+///   their specific use cases.
+///
+/// - The resolver associated with the `Graph` should be chosen based on the
+///   requirements of the application and its thread safety characteristics.
 pub struct Graph<Q, R> {
     /// The new map is used for all the queries in this iteration.
     /// This map always starts empty.
@@ -28,7 +125,7 @@ pub struct Graph<Q, R> {
     /// It's just a reference to the map from the previous iteration and
     /// so is very efficient.
     old: QueryNodeMap<Q, R>,
-    /// The resolver used to resolve queries. The resolver can have it's
+    /// The resolver used to resolve queries. The resolver can have its
     /// own state as long as it's Sync + Send.
     resolver: Box<dyn ResolveQuery<Q, R>>,
 }

948d71aac92bbbbd7d3a484c4078966255662183
NoahGav <noahgavdev@gmail.com>
2023-10-24 12:26:14 -0500

    Started the resolve algorithm.

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ pub mod map;
 #[derive(Debug)]
 struct Node<R> {
     result: R,
+    changed: bool,
+    // TODO: edges.
 }
 
 pub struct Graph<Q, R> {
@@ -24,7 +26,7 @@ pub struct Graph<Q, R> {
     resolver: Box<dyn ResolveQuery<Q, R>>,
 }
 
-impl<Q: Clone + Eq + Hash, R: Clone> Graph<Q, R> {
+impl<Q: Clone + Eq + Hash, R: Clone + Eq> Graph<Q, R> {
     pub fn new(resolver: impl ResolveQuery<Q, R> + 'static) -> Arc<Self> {
         Arc::new(Self {
             new: ConcurrentMap::new(),
@@ -43,9 +45,47 @@ impl<Q: Clone + Eq + Hash, R: Clone> Graph<Q, R> {
     }
 
     fn resolve(self: &Arc<Self>, q: Q) -> Node<R> {
-        let result = self.resolver.resolve(q);
+        let old = self.old.get(&q);
 
-        Node { result }
+        if let Some(old) = old {
+            // Since there was an old node we have to validate it.
+            let old_node = old.get();
+
+            if let Some(_old_node) = old_node {
+                // TODO: Since the old node is already resolved we actually validate
+                // TODO: the node. To do this, we traverse the graph upwards from this
+                // TODO: node to it's dependencies. We check if any of it's dependencies
+                // TODO: are changed. If none are we simply reuse the result from the
+                // TODO: old_node and set changed to false. Otherwise, we have to resolve
+                // TODO: the node again and then compare the new result with the old result.
+                todo!()
+            } else {
+                // Since the old node is not resolved yet we will just resolve
+                // it from scratch.
+                let result = self.resolver.resolve(q);
+
+                Node {
+                    // We need to check again if the node is still unresolved. Because
+                    // if it isn't we can set changed: old_result != result. Otherwise,
+                    // we always set changed to true.
+                    changed: match old.get() {
+                        Some(old_node) => result != old_node.result,
+                        None => true,
+                    },
+                    result,
+                }
+            }
+        } else {
+            // If the node isn't in the old map then the query is new and resolved
+            // from scratch.
+            let result = self.resolver.resolve(q);
+
+            Node {
+                result,
+                // Since this is a new node, changed is set to false.
+                changed: false,
+            }
+        }
     }
 
     pub fn increment(self: &Arc<Self>, resolver: impl ResolveQuery<Q, R> + 'static) -> Arc<Self> {
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ enum Query {
     Foo,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum QueryResult {
     Foo(String),
 }
@@ -35,22 +35,4 @@ fn main() {
     for thread in threads {
         thread.join().unwrap();
     }
-
-    // Ok, so the nodes will be stored as a once-cell. The reason for this
-    // is so that we do not hold a write lock into the map when resolving
-    // the node inside the get_or_insert value method.
-
-    // So, the way the concurrent dependency graph works is fairly simple.
-    // It has a valid map of nodes, and an invalid map of nodes. When querying
-    // the graph it does get_or_insert on the valid map and then get_or_init
-    // on the cell returned. The value in the cell function checks the value
-    // of the old map to see if it exists (using get so it doesn't block).
-    // If the value isn't ready we simply resolve the query from scratch.
-    // If the value is ready then we validate it recursively upwards (using
-    // it's edges). If the query was resolved from scratch we then attempt
-    // to get the old nodes value again, if it has something now we compare
-    // to the new value to see if the value actually changed. If it's still
-    // not there we set changed to true (this is because we cannot know
-    // whether or not the value is the same or not). If the node wasn't in
-    // the old map at all then we just resolve it like normal.
 }
